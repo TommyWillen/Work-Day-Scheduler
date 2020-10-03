ffcb77e62de94acfcd19f1a148bb63368effd1ee
Tommy Willen <tommyallen1215@gmail.com>
2020-10-03 11:58:11 -0700

    fixed storage issues, added clear button, and new day handler that clears each day.

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -1,64 +1,124 @@
-var arrIndex = [1,2,3,4,5]
-var arrName = ["Jani","Hege","Stale","Kai Jim","Borge"]
+let timeIndexData = {};
+let storedTimeData = localStorage.getItem("storedTimeInfo");
+let timeIndex;
+let timeData;
+if (!storedTimeData) {
+  timeIndex = $(".hour")
+    .map(function () {
+      return $(this).attr("value");
+    })
+    .get();
+  console.log(timeIndex);
 
-arrName.splice(2,1,"Lene");
-var result = {};
-arrIndex.forEach((key, i) => result[key] = arrName[i]);
-console.log(result);
+  timeData = $(".time-data")
+    .map(function () {
+      return $(this).text();
+    })
+    .get();
+  console.log(timeData);
 
-let timeIndex = $(".hour span").map(function(){
-  return parseInt($(this).text());
-}).get();
-console.log(timeIndex);
+  timeIndex.forEach((key, i) => (timeIndexData[key] = timeData[i]));
+  console.log(timeIndexData);
 
+  storedTimeData = JSON.stringify(timeIndexData);
+  localStorage.setItem("storedTimeInfo", storedTimeData);
+  console.log(storedTimeData);
+console.log(timeIndexData);
+} else {
+  timeIndexData = JSON.parse(storedTimeData);
+  console.log(storedTimeData);
+  console.log(timeIndexData);
+  timeIndex = Object.keys(timeIndexData);
+  console.log("timeIndex: " + timeIndex);
+  timeData = Object.values(timeIndexData);
+  console.log("timeData: " + timeData);
+}
 
-$(document).ready(function () {
-  let currentTime = parseInt(moment().format("HH"));
+// console.log($("textarea").innerHTML.toArray());
 
-  dateTimeSeconds();
+$("textarea").text(function(index){
+  return timeData[index];
+})
+// for (let i = 0; i < timeData.length; i++) {
 
-  let hourBlockTimes = $(".hour");
-  console.log(currentTime);
+// }
 
-  $(".hour").each(function () {
-    if (parseInt($(this).attr("value")) === currentTime) {
-      $(this).next().removeClass("past");
-      $(this).next().removeClass("future");
-      $(this).next().removeClass("present");
-      $(this).next().addClass("present");
-    } else if (parseInt($(this).attr("value")) > currentTime) {
-      $(this).next().removeClass("past");
-      $(this).next().removeClass("future");
-      $(this).next().removeClass("present");
-      $(this).next().addClass("future");
-    } else {
-      $(this).next().removeClass("past");
-      $(this).next().removeClass("future");
-      $(this).next().removeClass("present");
-      $(this).next().addClass("past");
-    }
+function dateTimeSeconds() {
+  let todayDateTime = moment().format("dddd, MMMM Do");
+  $("#currentDay").text(todayDateTime);
+  // setInterval(dateTimeSeconds, 500);
+}
+let currentTime = parseInt(moment().format("HH"));
 
-  //   $(".saveBtn").click(function(){
-  //     console.log("it worked!");
-  //     localStorage.setItem("TimeData", $(this).parent().prev().val())
-  //     // $(this).parent().prev().val();
-  // })
-    $(".container").on("click", saveHandler)
+dateTimeSeconds();
 
-    function saveHandler(event) {
-      event.preventDefault();
+let hourBlockTimes = $(".hour");
+console.log(currentTime);
 
-      if (event.target.matches("button")) {
-
-      }
-    }
-  });
-
-
-
-  function dateTimeSeconds() {
-    let todayDateTime = moment().format("dddd, MMMM Do, hh:mm:ss A");
-    $("#currentDay").text(todayDateTime);
-    setInterval(dateTimeSeconds, 500);
+$(".hour").each(function () {
+  if (parseInt($(this).attr("value")) === currentTime) {
+    $(this).next().removeClass("past");
+    $(this).next().removeClass("future");
+    $(this).next().removeClass("present");
+    $(this).next().addClass("present");
+  } else if (parseInt($(this).attr("value")) > currentTime) {
+    $(this).next().removeClass("past");
+    $(this).next().removeClass("future");
+    $(this).next().removeClass("present");
+    $(this).next().addClass("future");
+  } else {
+    $(this).next().removeClass("past");
+    $(this).next().removeClass("future");
+    $(this).next().removeClass("present");
+    $(this).next().addClass("past");
   }
 });
+
+
+
+$(".saveBtn").click(function () {
+  console.log("it worked!");
+let textAreaIndex = parseInt($(this).parent().prev().prev().attr("value"))-8;
+let textAreaValue = $(this).parent().prev().val();
+console.log(textAreaValue);
+
+timeData.splice(textAreaIndex,1,textAreaValue);
+console.log(timeData);
+
+timeIndex.forEach((key, i) => (timeIndexData[key] = timeData[i]));
+  console.log(timeIndexData);
+
+  storedTimeData = JSON.stringify(timeIndexData);
+  localStorage.setItem("storedTimeInfo", storedTimeData);
+  // localStorage.setItem("TimeData", $(this).parent().prev().val())
+  // $(this).parent().prev().val();
+});
+
+$(".clearBtn").click(clearAll);
+
+function clearAll() {
+  localStorage.removeItem("storedTimeInfo");
+  timeData = ["","","","","","","","","",""]
+  $("textarea").text(function(index){
+    return timeData[index];
+  });
+  location.reload();
+}
+
+var today = new Date(), lastUpdate;
+
+window.addEventListener( "mousemove", function () {
+  var time = new Date();
+  // If we haven't checked yet, or if it's been more than 30 seconds since the last check
+  if ( !lastUpdate || ( time.getTime() - lastUpdate.getTime() ) > 30000 ) {
+    // Set the last time we checked, and then check if the date has changed.
+    lastUpdate = time
+    if ( time.getDate() !== today.getDate() ) {
+      // If the date has changed, set the date to the new date, and refresh stuff.
+      today = time;
+
+      clearAll();
+      location.reload();
+    }
+  }
+} )
